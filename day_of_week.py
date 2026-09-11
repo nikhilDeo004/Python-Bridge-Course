9678274d7bc55646434b7bf697645ce6ebf94f71
nikhilDeo004 <nikhildeo004@gmail.com>
2026-09-11 15:40:48 +0530

    Day of Week

--- day_of_week.py
+++ day_of_week.py
@@ -1,15 +1,20 @@
-a=int(input("Please Enter a number : "))
-if(a==1):
-    print("Monday")
-elif(a==2):
-    print("Tuesday")    
-elif(a==3):
-    print("Wednesday")
-elif(a==4):
-    print("Thursday")
-elif(a==5):
-    print("Friday")
-elif(a==6):
-    print("Saturday")
-elif(a==7):
-    print("Sunday") 
+a = 0
+while a < 1 or a > 7:
+    a = int(input("Please Enter a number between 1 to 7: "))
+    if a < 1 or a > 7:
+        print("Please enter a valid number between 1 to 7.")
+    else:
+        if a == 1:
+            print("Monday")
+        elif a == 2:
+            print("Tuesday")
+        elif a == 3:
+            print("Wednesday")
+        elif a == 4:
+            print("Thursday")
+        elif a == 5:
+            print("Friday")
+        elif a == 6:
+            print("Saturday")
+        elif a == 7:
+            print("Sunday")
